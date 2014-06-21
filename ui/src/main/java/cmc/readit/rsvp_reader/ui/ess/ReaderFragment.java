7d017ecfffa88b78f98d21c43e9e21d7a25290b6
infm <agony404@gmail.com>
2014-06-22 02:11:14 +0300

    padding fixed - now it set from center, not from left

--- ui/src/main/java/cmc/readit/rsvp_reader/ui/ess/ReaderFragment.java
+++ ui/src/main/java/cmc/readit/rsvp_reader/ui/ess/ReaderFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
