2cb26f3736f3aac3c313716a9f2bfd25d4f2d87d
infm <agony404@gmail.com>
2014-06-25 15:58:18 +0300

    Constants.java created[2]

---
+++ ui/src/main/java/com/infm/readit/Constants.java
@@ -0,0 +1,14 @@
+package com.infm.readit;
+
+/**
+ * Created by infm on 6/25/14. Enjoy ;)
+ */
+public class Constants {
+    public static final String EXTRA_PATH = "path";
+    public static final String EXTRA_ROWID = "_id";
+    public static final String EXTRA_POSITION = "position";
+    public static final String EXTRA_TYPE = "source_type";
+    public static final String EXTRA_TEXT = "text";
+    public static final String EXTRA_HEADER = "header";
+    public static final String EXTRA_PERCENT = "percent_left";
+}
