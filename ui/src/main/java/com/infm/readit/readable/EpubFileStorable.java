5d91dbffba98fb0dacc7cd15d1b476109e6293f6
infm <agony404@gmail.com>
2014-07-02 21:39:47 +0300

    files added

---
+++ ui/src/main/java/com/infm/readit/readable/EpubFileStorable.java
@@ -0,0 +1,61 @@
+package com.infm.readit.readable;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.epub.EpubReader;
+
+/**
+ * Created by infm on 7/2/14. Enjoy ;)
+ */
+public class EpubFileStorable extends FileStorable {
+
+	private static final String LOGTAG = "EpubFileStorable";
+
+	public EpubFileStorable(){
+		extension = "epub";
+		type = TYPE_EPUB;
+	}
+
+	public void process(Context context){
+		Log.d(LOGTAG, "process() is called, type: epub");
+		try {
+			path = FileStorable.takePath(context, path);
+			if (path == null){
+				Log.d(LOGTAG, "path is null");
+				return;
+			}
+			Book book = (new EpubReader()).readEpub(new FileInputStream(path));
+			for (Resource res : book.getContents())
+				text.append(new String(res.getData()));
+			text = new StringBuilder(parseEpub(text.toString())); //NullPointerEx can be thrown
+
+			createRowData(context);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String parseEpub(String text){
+		Document doc = null;
+		try {
+			doc = Jsoup.parse(text);
+			title = doc.title();
+			return title + " | " + doc.select("p").text();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
---
+++ ui/src/main/java/com/infm/readit/readable/TxtFileStorable.java
@@ -0,0 +1,45 @@
+package com.infm.readit.readable;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * Created by infm on 7/2/14. Enjoy ;)
+ */
+public class TxtFileStorable extends FileStorable {
+
+	private static final String LOGTAG = "TxtFileStorable";
+
+	public TxtFileStorable(){
+		extension = "txt";
+		type = TYPE_TXT;
+	}
+
+	public void process(Context context){
+		Log.d(LOGTAG, "type: txt");
+		try {
+			path = FileStorable.takePath(context, path);
+			if (path == null){
+				Log.d(LOGTAG, "path is null");
+				return;
+			}
+			FileReader fileReader = new FileReader(path);
+			BufferedReader br = new BufferedReader(fileReader);
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null)
+				text.append(sCurrentLine).append('\n');
+			br.close();
+
+			createRowData(context);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
