584c0d7ecf5ca62371fc59246ec23b81fc03edf6
infm <agony404@gmail.com>
2014-09-06 23:53:56 +0300

    XML parser was added

--- ui/src/main/java/com/infmme/readily/xmlparser/XMLEvent.java
+++ ui/src/main/java/com/infmme/readily/xmlparser/XMLEvent.java
@@ -6,27 +6,117 @@ import android.util.Pair;
  * created on 8/26/14 by infm. Enjoy ;)
  */
 public class XMLEvent {
-	private Pair<Integer, Integer> domain;
+	private Pair<Long, Long> domain;
 	private int type;
+	private int closeType;
 
 	private String content;
+	private String contentType;
 	private String tagName;
 
 	public XMLEvent(int type){
-		domain = new Pair<Integer, Integer>(-1, -1);
+		content = "";
+		tagName = "";
+		domain = new Pair<Long, Long>((long) -1, (long) -1);
 		this.type = type;
+		generateCloseType();
 	}
 
-	public Pair<Integer, Integer> getDomain() { return domain; }
-	public int getEndPosition() { return domain.second; }
-	public int getStartPosition() { return domain.first; }
-	public int getType() { return type; }
-	public String getContent(){	return content; }
-	public String getTagName(){	return tagName;	}
+	public Pair<Long, Long> getDomain(){
+		return domain;
+	}
 
-	public void setEndPosition(int endPosition) { domain = new Pair<Integer, Integer>(domain.first, endPosition); }
-	public void setStartPosition(int startPosition) { domain = new Pair<Integer, Integer>(startPosition, domain.second); }
+	public long getEndPosition(){
+		return domain.second;
+	}
 
-	public void appendContent(char c) {	content += c; }
-	public void appendTagName(char c) { tagName += c; }
+	public void setEndPosition(long endPosition){
+		domain = new Pair<Long, Long>(domain.first, endPosition);
+	}
+
+	public long getStartPosition(){
+		return domain.first;
+	}
+
+	public void setStartPosition(long startPosition){
+		domain = new Pair<Long, Long>(startPosition, domain.first);
+	}
+
+	public int getType(){
+		return type;
+	}
+
+	public int getCloseType(){
+		return closeType;
+	}
+
+	public String getContent(){
+		return content;
+	}
+
+	public String getTagName(){
+		return tagName;
+	}
+
+	public String getContentType(){
+		return contentType;
+	}
+
+	public void setContentType(String contentType){
+		this.contentType = contentType;
+	}
+
+	public void clarifyTagType(int tagType){
+		if (type == XMLParser.TAG){
+			type = tagType;
+			generateCloseType();
+		}
+	}
+
+	public void appendContent(char c){
+		content += c;
+	}
+
+	public void cutLastContentChar(){
+		content = content.substring(0, content.length() - 1);
+	}
+
+	public void appendTagName(char c){
+		tagName += c;
+	}
+
+	public void cutLastTagChar(){
+		tagName = tagName.substring(0, tagName.length() - 1);
+	}
+
+	@Override
+	public String toString(){
+		return "domain: from " + domain.first + " to " + domain.first +
+				"; type: " + XMLParser.getTypeName(type) +
+				"; close type: " + XMLParser.getTypeName(closeType) +
+				"; content: " + content +
+				"; tag name: " + tagName;
+	}
+
+	private void generateCloseType(){
+		switch (type){
+			case XMLParser.DOCUMENT_START:
+				closeType = XMLParser.DOCUMENT_CLOSE;
+				break;
+			case XMLParser.TAG_START:
+				closeType = XMLParser.TAG_CLOSE;
+				break;
+			case XMLParser.CONTENT:
+				closeType = XMLParser.TAG;
+				break;
+			case XMLParser.EMPTINESS:
+				closeType = XMLParser.TAG_START;
+				break;
+			case XMLParser.TAG_SINGLE:
+				closeType = type;
+				break;
+			default:
+				closeType = -1;
+		}
+	}
 }
--- ui/src/main/java/com/infmme/readily/xmlparser/XMLParser.java
+++ ui/src/main/java/com/infmme/readily/xmlparser/XMLParser.java
@@ -2,6 +2,8 @@ package com.infmme.readily.xmlparser;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Stack;
 
 /**
@@ -11,84 +13,250 @@ public class XMLParser {
 	/* Event types */
 	public static final int DOCUMENT_START = 0;
 	public static final int DOCUMENT_CLOSE = 1;
+	public static final int TAG = 665;
 	public static final int TAG_START = 666;
 	public static final int TAG_CLOSE = 667;
+	public static final int TAG_SINGLE = 668;
 	public static final int EMPTINESS = 31415;
 	public static final int CONTENT = 228;
-	private FileInputStream fis;
-	private String encoding;
+	private InputStreamReader isr;
 	private XMLEvent currentEvent;
-	private Stack<XMLEvent> eventStack;
+	private Stack<XMLEvent> tagStack = new Stack<XMLEvent>();
 
-	public void setInput(FileInputStream fis, String encoding){
-		this.fis = fis;
-		this.encoding = encoding;
+	private int currentInt = -1, nextInt = -1;
+	private long position = 0;
+
+	public static String getTypeName(int type){
+		switch (type){
+			case DOCUMENT_START:
+				return "DOCUMENT_START";
+			case DOCUMENT_CLOSE:
+				return "DOCUMENT_CLOSE";
+			case TAG:
+				return "TAG";
+			case TAG_START:
+				return "TAG_START";
+			case TAG_CLOSE:
+				return "TAG_CLOSE";
+			case TAG_SINGLE:
+				return "TAG_SINGLE";
+			case EMPTINESS:
+				return "EMPTINESS";
+			case CONTENT:
+				return "CONTENT";
+			default:
+				return "Illegal argument";
+		}
 	}
 
-	public XMLEvent next(){
+	public void setInput(FileInputStream fis, String encoding){
+		try {
+			position = fis.getChannel().position();
+			isr = new InputStreamReader(fis, encoding);
+		} catch (UnsupportedEncodingException e){
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void setInput(FileInputStream fis){
+		setInput(fis, "UTF-8");
+	}
+
+	public XMLEvent next() throws IOException{
+		currentEvent = null;
+		processEvent();
 		return currentEvent;
 	}
 
-	private void processEvent() throws IOException{
-		int type = -1;
-		int currentInt = fis.read();
-		int nextInt = fis.read();
-		if (eventStack.isEmpty()){
+	private void readNext() throws IOException{
+		currentInt = nextInt;
+		nextInt = isr.read();
+		position++;
+	}
 
-		} else {
-			int prevType = eventStack.lastElement().getType();
-			if (prevType != DOCUMENT_START){
-				switch ((char) currentInt){
-					case '<':
-						if (char)
-				}
-			} else {
-				currentEvent = new XMLEvent(EMPTINESS);
-			}
+	private void processEvent() throws IOException{
+		int type;
+		if (currentInt == -1){
+			currentInt = isr.read();
+			nextInt = isr.read();
+			position += 2;
 		}
-		while (breakClause(type, currentInt, nextInt)){
-			switch (type){
+		if (Character.isWhitespace(nextInt))
+			readNext();
+		while (Character.isWhitespace(currentInt))
+			readNext();
+		if (currentInt == '<'){
+			if (nextInt == '/'){
+				currentEvent = new XMLEvent(type = TAG_CLOSE);
+				readNext();
+			} else if (nextInt == '?'){
+				currentEvent = new XMLEvent(type = DOCUMENT_START);
+				readNext();
+			} else {
+				currentEvent = new XMLEvent(type = TAG);
+			}
+		} else { //consider random position as pointing to content of text;
+			currentEvent = new XMLEvent(type = CONTENT);
+		}
+		currentEvent.setStartPosition(position);
+/*			!????
+		if (tagStack.isEmpty()){
+			if (currentInt == '<'){
+				if (nextInt == '/') {
+					currentEvent = new XMLEvent(type = TAG_CLOSE);
+					readNext();
+				} else if (nextInt == '?'){
+					currentEvent = new XMLEvent(type = DOCUMENT_START);
+					readNext();
+				} else {
+					currentEvent = new XMLEvent(type = TAG);
+				}
+			} else { //consider random position as pointing to content of text;
+				currentEvent = new XMLEvent(type = CONTENT);
+			}
+		} else {
+			int prevType = tagStack.lastElement().getType();
+			switch (prevType){
 				case DOCUMENT_START:
-					currentEvent.appendTagName( (char) currentInt);
+					currentEvent = new XMLEvent(type = EMPTINESS);
 					break;
 				case DOCUMENT_CLOSE:
-					currentEvent.appendTagName( (char) currentInt);
-					break;
+					throw new IllegalArgumentException("WTF, event has appeared after DOCUMENT_CLOSE");
 				case TAG_START:
-					currentEvent.appendTagName( (char) currentInt);
+					if (currentInt == '<') {
+						if (nextInt == '/')
+							currentEvent = new XMLEvent(type = TAG_CLOSE);
+						else
+							currentEvent = new XMLEvent(type = TAG);
+					} else {
+						currentEvent = new XMLEvent(type = CONTENT);
+					}
 					break;
 				case TAG_CLOSE:
-					currentEvent.appendTagName( (char) currentInt);
+					throw new IllegalArgumentException("TAG_CLOSE is in tagStack");
+				case CONTENT:
+					if (currentInt == '<') {
+						currentEvent = new XMLEvent(type = TAG);
+						break;
+					} else {
+						throw new IllegalArgumentException("non-TAG after EMPTINESS");
+					}
+				case EMPTINESS:
+					if (currentInt == '<') {
+						currentEvent = new XMLEvent(type = TAG);
+						break;
+					} else {
+						throw new IllegalArgumentException("non-TAG after EMPTINESS");
+					}
+			}
+		}
+*/
+		do {
+			readNext();
+			updateType(type, currentInt, nextInt);
+			type = currentEvent.getType();
+			switch (type){
+				case DOCUMENT_START:
+					currentEvent.appendTagName((char) currentInt);
+					break;
+				case DOCUMENT_CLOSE:
+					currentEvent.appendTagName((char) currentInt);
+					break;
+				case TAG:
+					currentEvent.appendTagName((char) currentInt);
+					break;
+				case TAG_START:
+					currentEvent.appendTagName((char) currentInt);
+					break;
+				case TAG_CLOSE:
+					currentEvent.appendTagName((char) currentInt);
+					break;
+				case TAG_SINGLE:
+					currentEvent.appendTagName((char) currentInt);
 					break;
 				case CONTENT:
-					currentEvent.appendContent( (char) currentInt);
+					currentEvent.appendContent((char) currentInt);
 					break;
 				case EMPTINESS:
 					break;
 				default:
 					throw new IllegalArgumentException("type doesn't exist");
 			}
+		} while (!breakClause(type, currentInt, nextInt));
+
+		switch (type){
+			case CONTENT:
+				if (currentEvent.getContent().length() > 1){
+					readNext();
+					if (!tagStack.isEmpty())
+						currentEvent.setContentType(tagStack.lastElement().getTagName());
+				} else {
+					currentEvent = null;
+					processEvent();
+				}
+				break;
+			case TAG:
+				currentEvent.clarifyTagType(TAG_START);
+				tagStack.push(currentEvent);
+				readNext();
+				break;
+			case TAG_CLOSE:
+				if (!tagStack.empty() && tagStack.lastElement().getTagName().equals(currentEvent.getTagName()))
+					tagStack.pop();
+				readNext();
+				readNext();
+				break;
+			case TAG_START:
+				tagStack.push(currentEvent);
+				readNext();
+				readNext();
+				break;
+			case DOCUMENT_CLOSE:
+				if (!tagStack.empty() && tagStack.lastElement().getTagName().equals(currentEvent.getTagName()))
+					tagStack.pop();
+				break;
+			case DOCUMENT_START:
+				currentEvent.cutLastTagChar();
+				tagStack.push(currentEvent);
+				readNext();
+				readNext();
+				break;
 		}
+
+		currentEvent.setEndPosition(position);
 	}
 
 	private boolean breakClause(int type, int currentInt, int nextInt){
-		char currentChar = (char) currentInt;
-		char nextChar = (char) nextInt;
 		switch (type){
 			case CONTENT:
-				return currentChar == '<';
+				return currentInt == '<' || nextInt == '<';
 			case EMPTINESS:
-				return currentChar == '<';
+				return currentInt == '<' || nextInt == '<';
+			case TAG:
+				return nextInt == '>';
 			case TAG_START:
-				return currentChar == '>';
+				return nextInt == '>';
 			case TAG_CLOSE:
-				return currentChar == '/' && nextChar == '>';
+				return nextInt == '>';
+			case TAG_SINGLE:
+				return currentInt == '/' && nextInt == '>';
 			case DOCUMENT_START:
-				return currentChar == '>';
+				return currentInt == '?' && nextInt == '>';
 			case DOCUMENT_CLOSE:
-				return currentChar == '/' && nextChar == '>';
+				return nextInt == '>';
 			default:
 				throw new IllegalArgumentException("type doesn't exist");
+		}
+	}
+
+	private void updateType(int type, int currentInt, int nextInt){
+		switch (type){
+			case TAG:
+				if (nextInt == '>' && currentInt == '/')
+					currentEvent.clarifyTagType(TAG_SINGLE);
+				break;
 		}
 	}
 }
