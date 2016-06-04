36cb975981a0ca3c1065ad00deb8e16b32a74469
Illia Syniuhin <syniuhin@gmail.com>
2016-06-04 17:45:01 +0300

    Compiled.

--- ui/src/main/java/com/infmme/readilyapp/util/BaseActivity.java
+++ ui/src/main/java/com/infmme/readilyapp/util/BaseActivity.java
@@ -1,21 +1,18 @@
 package com.infmme.readilyapp.util;
 
 import android.support.v7.app.ActionBarActivity;
-import com.flurry.android.FlurryAgent;
 
 /**
  * created on 7/15/14 by infm. Enjoy ;)
  */
 public class BaseActivity extends ActionBarActivity {
-	@Override
-	protected void onStart(){
-		super.onStart();
-		FlurryAgent.onStartSession(this, "6CNDCMYSWHYDFYDVKDMD");
-	}
+  @Override
+  protected void onStart() {
+    super.onStart();
+  }
 
-	@Override
-	protected void onStop(){
-		super.onStop();
-		FlurryAgent.onEndSession(this);
-	}
+  @Override
+  protected void onStop() {
+    super.onStop();
+  }
 }
